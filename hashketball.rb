c3fc347bbb3dc03753523e4f61e4f78cc1909506
Reid Watson <j.watsonreid@gmail.com>
2020-09-11 05:08:30 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+require "pry"
+
 def game_hash
   {
     home: {
@@ -126,4 +128,85 @@ def game_hash
   }
 end
 
+
 # Write code here
+
+def num_points_scored(player_name)
+  
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |player|
+      if player[:player_name] == player_name
+        return player[:points]
+      end
+    end
+  end
+end
+
+
+def shoe_size(name)
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |player|
+      if player[:player_name] == name
+        return player[:shoe]
+      end
+    end
+  end
+end
+
+def team_colors(team)
+  if team.downcase == "charlotte hornets" 
+    return game_hash[:away][:colors]
+  else return game_hash[:home][:colors]
+  end
+end
+
+def team_names
+  game_hash.map do |team, team_data|
+    team_data[:team_name]
+  end
+end
+
+def player_numbers(input)
+  new_array = []
+  game_hash.each do |team, team_data|
+    if team_data[:team_name] == input 
+      team_data.each do |key, value|
+        if key == :players
+          value.each do |player|
+          new_array.push(player[:number])
+          end
+        end
+      end
+    end
+  end
+  return new_array
+end
+
+def player_stats(input)
+  game_hash.each do |team, team_data|
+    team_data.each do |key, value|
+      if key == :players
+        value.each do |player|
+          if input == player[:player_name]
+            return player
+          end
+        end
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds
+  big_shoe = 0
+  rebounds = 0
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  return rebounds
+end
+  
